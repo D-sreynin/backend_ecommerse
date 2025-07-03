cdf3f7ac51a2d533eb9d0220ca8070123232e97b
satoryuk <your_email@example.comthong.vathana.2822@rupp.edu.kh>
2025-07-03 22:44:19 +0700

    push

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*',
-        'https://frontend-ecommerse-steel.vercel.app/',
+    'allowed_origins' => [
+        'http://localhost:5173',
         'http://localhost:3000',
+        'https://frontend-ecommerse-steel.vercel.app',
+        'https://backendecommerse-production-75ff.up.railway.app'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
